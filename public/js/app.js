0200cc81490b0c8af575f8b617bd607590df10a6
Rebirth-celebration <786742044@qq.com>
2020-10-19 07:57:17 +0000

    切割头部和尾部子视图

--- public/js/app.js
+++ public/js/app.js
@@ -37339,8 +37339,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/Code/weiboo/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/Code/weiboo/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\78674\Code\weiboo\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\78674\Code\weiboo\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
